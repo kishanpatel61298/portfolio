552333428469a03aafb74a5c96ca5499e1c3110b
Kishan Patel <32492255+kishanpatel61298@users.noreply.github.com>
2022-07-08 23:53:26 -0300

    Update canvas.js

--- assets/js/canvas.js
+++ assets/js/canvas.js
@@ -5,7 +5,7 @@ canvas.height = window.innerHeight;
 
 var c=canvas.getContext('2d');
 
-/*for(var i=0;i<100;i++)
+/*for(var i=0;i<100;i++) 
 {
 	var x=Math.random()*window.innerWidth;
 	var y=Math.random()*window.innerHeight;
